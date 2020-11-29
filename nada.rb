4e3d9e8014cabdcace5eb32b5bcb3d084217fb6d
Juan Luis Cabello <juan.luiscoropeza@gmail.com>
2020-11-28 22:28:35 -0300

    last commit

---
+++ nada.rb
@@ -0,0 +1,6 @@
+def obtener_nombre
+    nombre = gets
+end
+
+nombre = obtener_nombre
+puts "Hola, #{nombre}! Que tal?"
---
+++ retorno.rb
@@ -0,0 +1,6 @@
+def transformar_a_farenheit(f)
+    celcius = (f + 40) / 1.8 - 40
+    return celcius
+end
+
+puts transformar_a_farenheit(180)
